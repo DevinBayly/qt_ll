de5c8624551ee46b1666fe68a5cab904110a8e4b
DevinBayly <baylyd@email.arizona.edu>
2021-04-28 13:56:17 -0700

    increased to 4 sub trees

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,58 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-
 #[derive(Debug)]
 enum El<T> {
     None,
-    Some(Rc<RefCell<Box<(T,El<T>)>>>)
+    Some(Rc<RefCell<Box<T>>>),
 }
 
 impl<T> El<T> {
-    fn new_part(data:T) -> Rc<RefCell<Box<(T,El<T>)>>> {
-        Rc::new(RefCell::new(Box::new((data,El::<T>::None))))
+    fn new_part(data: T) -> Rc<RefCell<Box<T>>> {
+        Rc::new(RefCell::new(Box::new(data)))
     }
 }
 #[derive(Debug)]
 struct Tree {
-    val:f32,
+    val: f32,
 }
 
 #[derive(Debug)]
 struct TwoTree {
-    val:f32,
-    left:El::<TwoTree>,
-    right:El::<TwoTree>,
+    val: f32,
+    ne: El<TwoTree>,
+    nw: El<TwoTree>,
+    se: El<TwoTree>,
+    sw: El<TwoTree>,
 }
 
+impl TwoTree {
+    fn new(v: f32) -> Self {
+        TwoTree {
+            val: v,
+            ne: El::None,
+            nw: El::None,
+            se: El::None,
+            sw: El::None,
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
-    let head = El::new_part(Tree{
-        val:0.0
-    });
+    let head = El::new_part(TwoTree::new(0.0));
     // make a copy of head via rc clone,
     let mut walker = Rc::clone(&head);
     // then loop and add to it
     for i in 0..20 {
         // make a new Element then add it
-        let new_ele = Rc::new(RefCell::new(Box::new(
-            (
-            Tree {
-                val:i as f32,
-            },El::<Tree>::None
-            )  
-        )));
+        let tree_part = TwoTree::new(i as f32);
+        let new_ele = El::new_part(tree_part);
         let next_walker = Rc::clone(&new_ele);
         // replace the none at the end of walker
-        walker.borrow_mut().1 = El::<Tree>::Some(new_ele);
+        walker.borrow_mut().ne = El::Some(new_ele);
         // make walker = new_ele
         walker = next_walker;
     }
-    println!("result {:#?}",head);
-
+    println!("result {:?}", head);
 }
