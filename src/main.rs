6f68f21f41be0bb798f196ef8a842d712c25323c
DevinBayly <baylyd@email.arizona.edu>
2021-04-29 13:55:34 -0700

    looks like this is a related thing to the overlap margin

--- src/main.rs
+++ src/main.rs
@@ -59,9 +59,9 @@ struct BB {
     h: f32,
     c: PT,
     t: f32,
-    r: f32,
     b: f32,
     l: f32,
+    r: f32,
 }
 
 impl BB {
@@ -116,60 +116,60 @@ impl QT {
     fn add_point(&mut self, o: PT, leafs: &mut Vec<Rc<RefCell<QT>>>) {
         // these are the children we might add the point to
         // start with head and proceed
-        //println!("num leafs {:?}",leafs.len());
+        ////println!("num leafs {:?}",leafs.len());
         let mut new_leafs = vec![];
         println!("leafs len{}",leafs.len());
         if !self.subdiv {
             if self.points.len() < self.capacity && self.bb.contains(&o) {
-                ////println!("beginning {:?}", self.points);
+                //////println!("beginning {:?}", self.points);
                 self.points.push(o);
             } else {
                 self.points.push(o.clone());
                 self.subdivide(&mut new_leafs);
                 *leafs = new_leafs;
-                ////println!("after subdiv head {:?}", self.points);
-                ////println!("self is subdivided {:?}",self.subdiv);
-                ////println!("self is {:#?}",self);
+                //////println!("after subdiv head {:?}", self.points);
+                //////println!("self is subdivided {:?}",self.subdiv);
+                //////println!("self is {:#?}",self);
             }
         } else {
             // unpack via popping from the leafs, but add them to the new leafs as we go under certain circumstances
-            //println!("first leafs {:?}",leafs);
+            ///println!("first leafs {:?}",leafs);
             while let Some(candidate_rc) = leafs.pop() {
-            //println!("looping leafs {:?}",leafs);
-                ////println!("candidate len is {:?}",candidates.len());
+            ////println!("looping leafs {:?}",leafs);
+                //////println!("candidate len is {:?}",candidates.len());
                 let mut candidate = candidate_rc.borrow_mut();
-                println!("candidate is {:?}",candidate);
+                //println!("candidate is {:?}",candidate);
                 if candidate.bb.contains(&o) {
                     if !candidate.subdiv {
                         // if capacity isn't full and haven't subdivided
                         if candidate.points.len() < candidate.capacity {
-                        println!("candidate  {:#?}contains {:?}",candidate,o);
+                        //println!("candidate  {:#?}contains {:?}",candidate,o);
                             candidate.points.push(o.clone());
                             // we should keep this leaf in our listing
                             new_leafs.push(candidate_rc.clone());
-                            //println!("new leafs {:?}",new_leafs);
+                            ////println!("new leafs {:?}",new_leafs);
                         } else {
                             // this is the complex spot, call on the subdivide for our candidate and spread the existing points between the children
                             // this is the end of this path too
                             // temporarily go above cap so that we can loop over the cap +1 points when redistributing
-                            println!("candidate  {:#?} needs to subdivide {:?}",candidate,o);
+                            //println!("candidate  {:#?} needs to subdivide {:?}",candidate,o);
                             candidate.points.push(o.clone());
                             candidate.subdivide(&mut new_leafs);
                         }
                         // else if we are at cap and haven't subdivided
                     }
                 } else {
-                    println!("candidate  {:#?}doesn't contain {:?}",candidate,o);
+                    //println!("candidate  {:#?}doesn't contain {:?}",candidate,o);
                     new_leafs.push(candidate_rc.clone());
                 }
                 // slowly deplete the list of candidates while we add
             }
-            //println!("new leafs end{:?}",new_leafs);    
+            ////println!("new leafs end{:?}",new_leafs);    
             *leafs = new_leafs;
         }
     }
     fn subdivide(&mut self, leafs_vec: &mut Vec<Rc<RefCell<QT>>>) {
-        println!("subdividing {:?} next leafs {:?}",self,leafs_vec);
+        println!("subdividing {:?}",self.bb.c);
         // make the 4 new children to replace the None's
         // pay special attention to the calculation of BB's for each
         self.subdiv = true;
@@ -222,8 +222,8 @@ impl QT {
         leafs_vec.push(rc_wrap_se.clone());
         leafs_vec.push(rc_wrap_nw.clone());
         leafs_vec.push(rc_wrap_sw.clone());
-        println!("in subdivide leafs_vec is {:#?}",leafs_vec);
-        //println!("leaf vec in subdivide {:?}",leafs_vec);
+        //println!("in subdivide leafs_vec is {:#?}",leafs_vec);
+        ////println!("leaf vec in subdivide {:?}",leafs_vec);
         self.ne = El::Some(rc_wrap_ne.clone());
         self.se = El::Some(rc_wrap_se.clone());
         self.nw = El::Some(rc_wrap_nw.clone());
@@ -248,44 +248,34 @@ impl QT {
 // still needs initial setup and random point creation as we loop
 fn main() {
     //////println!("Hello, world!");
-    //let max_number = args().nth(1).unwrap().parse::<usize>().unwrap();
-    //let mut thread = rand::thread_rng();
-    //let mut data = vec![];
-    //for i in 0..max_number {
-    //    data.push(PT::new(thread.gen::<f32>(), thread.gen::<f32>()));
-    //}
-    ////thread.fill_data(&mut rand_data);
-    //let mut x_ext = Extent::new();
-    //let mut z_ext = Extent::new();
-    //for pt in data.iter() {
-    //    x_ext.comp(pt.x);
-    //    z_ext.comp(pt.y);
-    //}
-    //// center should be the mid point which is (max - min)/2 + min
-    //let w = x_ext.max - x_ext.min;
-    //let h = z_ext.max - z_ext.min;
-    //let c = PT::new(
-    //    (x_ext.max - x_ext.min) / 2.0 + x_ext.min,
-    //    (z_ext.max - z_ext.min) / 2.0 + z_ext.min,
-    //);
-    let head = El::new_part(QT::new(PT::new(0.0,0.0), 8.0, 8.0, 0.0, 4));
+    let max_number = args().nth(1).unwrap().parse::<usize>().unwrap();
+    let mut thread = rand::thread_rng();
+    let mut data = vec![];
+    for i in 0..max_number {
+        data.push(PT::new(thread.gen::<f32>(), thread.gen::<f32>()));
+    }
+    //thread.fill_data(&mut rand_data);
+    let mut x_ext = Extent::new();
+    let mut z_ext = Extent::new();
+    for pt in data.iter() {
+        x_ext.comp(pt.x);
+        z_ext.comp(pt.y);
+    }
+    // center should be the mid point which is (max - min)/2 + min
+    let w = x_ext.max - x_ext.min;
+    let h = z_ext.max - z_ext.min;
+    let c = PT::new(
+        (x_ext.max - x_ext.min) / 2.0 + x_ext.min,
+        (z_ext.max - z_ext.min) / 2.0 + z_ext.min,
+    );
+    let head = El::new_part(QT::new(c, w, h, w/200.0 , 4));
     let head_ref = Rc::clone(&head);
     let mut leafs = vec![];
-    // in between 2 and 4
-    let data = [
-        PT::new(2.5,3.3),
-        PT::new(2.5,3.3),
-        PT::new(3.1,3.3),
-        PT::new(3.2,3.3),
-        PT::new(3.0,3.5),
-        PT::new(3.0,3.5),
-    ];
     for (i, pt) in data.iter().enumerate() {
         println!("point processed {:?} {:?}", pt, i);
         head_ref.borrow_mut().add_point(pt.clone(), &mut leafs);
-        println!("head is {:#?}", head_ref);
     }
-    println!("head {:#?}",head);
+    //println!("head {:?}",head);
     // iterate over the head and output data to draw the tree
 
 
